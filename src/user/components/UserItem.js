6af6e1cc150ff884a3db9d286d832c73236e0032
Shakila Marando <marandoshakila1@gmail.com>
2020-12-10 13:49:19 -0800

    first commit

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./UserItem.css";
+import Avatar from "../../shared/UIElements/Avatar";
+
+const UserItem = (props) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <Avatar image={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default UserItem;
